9bb72308573f9a7e3ce6135ce89d7bd899e74776
JeshuaOstnm <143622511+JeshuaOstnm@users.noreply.github.com>
2023-09-08 08:37:17 -0600

    Add files via upload

---
+++ AlreaTriangulo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.operaciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AlreaTriangulo {
+     public static void main(String[] args) {
+         
+         int base = 15;
+         int altura = 30;
+         
+         int area = (base * altura)/2;
+         
+         System.out.println("El area del triándulo es: " + area);
+         
+         
+     }
+}
---
+++ Operaciones.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operaciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Operaciones {
+
+    public static void main(String[] args) {
+       /** int numero1 = 10;
+        int numero2 = 0;
+        
+        int resultado = numero1 / numero2;
+        System.out.println(resultado);
+        **/
+       
+       /**int [] arr= {1,2,3};
+        System.out.println(arr [0]);
+        * **/
+       
+       int numeroA = 16;
+       int numeroB = 4;
+       
+       int suma = numeroA + numeroB;
+       int resta = numeroA - numeroB;
+       int multiplicacion = numeroA * numeroB;
+       int division = numeroA / numeroB;
+       
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicación es: " + multiplicacion);
+        System.out.println("La división es: " + division);
+       
+ 
+    }
+}
